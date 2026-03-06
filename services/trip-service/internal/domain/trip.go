86e270cb954bcdbd77e394ed94f245e2fa0dfb6d
Abishek Prasad <abishek.123.prasad@gmail.com>
2026-03-06 14:16:14 +0000

    created trip.go

---
+++ services/trip-service/internal/domain/trip.go
@@ -0,0 +1,31 @@
+package domain
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+
+type RideFareModel struct {
+	ID 					primitive.ObjectID
+	UserID 				string
+	PackageSlug 		string
+	TotalPriceInCents	float64
+}
+
+type TripModel struct {
+	ID primitive.ObjectID
+	UserID string
+	Status string
+	RideFare RideFareModel
+
+}
+
+type TripRepository interface {
+	CreateTrip(ctx context.Context , trip *TripModel) (*TripModel , error)
+}
+
+type TripService interface {
+	CreateTrip(ctx context.Context , fare *RideFareModel) (*TripModel , error)
+}
